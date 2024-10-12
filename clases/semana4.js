a917b5bef62b8236ab1855db6c8bede8b1abf8ef
klealufide <kleal40321@ufide.ac.cr>
2024-10-12 02:14:15 -0600

    Javascript

---
+++ clases/semana4.js
@@ -0,0 +1,66 @@
+let total = 50;
+
+console.log(total);
+
+function suma(a, b) {
+    let total = a + b; // ambito local
+    console.log(total);
+}
+
+let numero1 = 1;
+let numero2 = 6;
+suma(numero1, numero2);
+
+console.log(total); // global
+
+let nombre = "Karol Leal"; // string
+let edad = 34; //int
+let altura = 1.60; // float
+let esProfesor = true; // boleano
+
+const edadMayor = 18;
+
+
+
+nombre += " Rojas"
+
+console.log(nombre);
+console.log(edad);
+console.log(altura);
+console.log(esProfesor);
+
+console.log("-----------------------------------");
+console.log(nombre.length);
+console.log(nombre.startsWith("Ka"));
+console.log(edad.toString());
+console.log(esProfesor.toString());
+
+
+
+if (edad > edadMayor) {
+    console.log("es mayor de edad");
+} else {
+    console.log("es menor edad");
+}
+
+
+edad = 10;
+let mensaje = edad > edadMayor ? "Es mayor de edad" : "Es menor de edad";
+console.log(mensaje);
+
+let color = "rojo";
+switch (color) {
+    case 'rojo':
+        console.log("detengase");
+        break;
+    case 'amarillo':
+        console.log("precaucion");
+        break;
+    case 'verde':
+        console.log("siga");
+        break;
+    default:
+        console.log("color no definido");
+        break;
+
+}
